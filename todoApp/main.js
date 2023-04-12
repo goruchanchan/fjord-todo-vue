8d164540d465e9958825db24cb640cbc661d6fd0
goruchan <ppcfr481@yahoo.co.jp>
2023-04-12 23:53:10 +0900

    update ToDo追加機能実装

--- todoApp/main.js
+++ todoApp/main.js
@@ -1,6 +1,23 @@
 let vm = new Vue({
   el: "#list",
   data: {
-    items: localStorage,
+    newTodo: "",
+    todos: localStorage,
+  },
+  methods: {
+    addTodo() {
+      localStorage.setItem(localStorage.length + 1, this.newTodo);
+      this.newTodo = "";
+    },
+    sortLocalStorage() {
+      return Object.keys(localStorage)
+        .sort((a, b) => a - b)
+        .map((key) => {
+          return {
+            key: key,
+            value: localStorage.getItem(key),
+          };
+        });
+    },
   },
 });
