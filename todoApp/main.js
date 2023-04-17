e66357967865e579dfed59eb52cb5905eb4910ac
goruchan <ppcfr481@yahoo.co.jp>
2023-04-17 23:20:27 +0900

    update 削除機能導入

--- todoApp/main.js
+++ todoApp/main.js
@@ -3,30 +3,26 @@ let vm = new Vue({
   data: {
     newTodo: "",
     errorMessage: "",
-    todos: localStorage,
+    todos: [],
+  },
+  mounted() {
+    if (localStorage.length > 0 && localStorage.todos !== '')
+      this.todos = localStorage.getItem("todos").split(",");
   },
   methods: {
     addTodo() {
       if (this.newTodo.length !== 0) {
-        localStorage.setItem(localStorage.length + 1, this.newTodo);
+        this.todos.push(this.newTodo);
+        localStorage.setItem("todos", this.todos);
         this.newTodo = "";
         this.errorMessage = "";
       } else {
         this.errorMessage = "タスク内容を入力してください";
       }
     },
-    removeTodo(item) {
-      localStorage.removeItem(item.key);
-    },
-    sortLocalStorage() {
-      return Object.keys(localStorage)
-        .sort((a, b) => a - b)
-        .map((key) => {
-          return {
-            key: key,
-            value: localStorage.getItem(key),
-          };
-        });
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      localStorage.setItem("todos", this.todos);
     },
     hasInput() {
       return this.newTodo.length > 0;
