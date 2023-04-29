510ff45a8189b6393ce500fb648947ab693b8078
goruchan <ppcfr481@yahoo.co.jp>
2023-04-30 07:03:53 +0900

    update 削除機能の入力をオブジェクトに変更

--- todoApp/main.js
+++ todoApp/main.js
@@ -33,8 +33,8 @@ Vue.createApp({
         this.errorMessage = "ToDoを入力してください";
       }
     },
-    removeTodo(index) {
-      this.todoItems.splice(index, 1);
+    removeTodo(target) {
+      this.todoItems = this.todoItems.filter((todo) => todo !== target);
       localStorage.setItem("todoItems", JSON.stringify(this.todoItems));
     },
     isEdit(todo) {
