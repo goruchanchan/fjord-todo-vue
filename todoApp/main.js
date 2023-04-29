77c4aa46aa180b7b07fb47b83ac23026cb7d3355
goruchan <ppcfr481@yahoo.co.jp>
2023-04-30 07:04:44 +0900

    update 引数名変更

--- todoApp/main.js
+++ todoApp/main.js
@@ -33,8 +33,8 @@ Vue.createApp({
         this.errorMessage = "ToDoを入力してください";
       }
     },
-    removeTodo(target) {
-      this.todoItems = this.todoItems.filter((todo) => todo !== target);
+    removeTodo(targetTodo) {
+      this.todoItems = this.todoItems.filter((todo) => todo !== targetTodo);
       localStorage.setItem("todoItems", JSON.stringify(this.todoItems));
     },
     isEdit(todo) {
