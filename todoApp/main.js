6c38c7f8ef6d0b3c9ec566fdb19baa14a20a1d51
goruchan <ppcfr481@yahoo.co.jp>
2023-05-07 22:03:11 +0900

    clean 各データは固有IDを持つように修正

--- todoApp/main.js
+++ todoApp/main.js
@@ -16,16 +16,14 @@ Vue.createApp({
   },
   beforeUpdate() {
     localStorage.setItem("todoItems", JSON.stringify(this.todoItems));
+    localStorage.setItem("maxId", this.maxId);
   },
   methods: {
     loadTodoItems(localStorageTodoItems) {
       this.todoItems = JSON.parse(localStorageTodoItems);
     },
     loadMaxId() {
-      const ids = this.todoItems.map(({ id }) => id);
-      const maxId = ids.reduce((a, b) => Math.max(a, b), -Infinity);
-      // リロード後にコンテンツがない場合は０スタート、それ以外の場合はIDを上書きしないように最大値の次からスタート
-      this.maxId = ids.length === 0 ? 0 : maxId + 1;
+      this.maxId = localStorage.getItem("maxId");
     },
     add(newTodoText) {
       this.tryingAddTodo = true;
